fe1ce94ec334f94538b86e6189b42e508b8b813c
Anshuman <anshuman844@gmail.com>
2023-03-14 21:44:48 +0530

    feat: Add Navmenu component

---
+++ src/components/navmenu/index.tsx
@@ -0,0 +1,3 @@
+import NavMenu from "./navmenu";
+
+export { NavMenu };
---
+++ src/components/navmenu/navmenu.tsx
@@ -0,0 +1,17 @@
+import styles from "@/styles/components/Navmenu.module.css";
+import Link from "next/link";
+type NavMenuProps = {
+  links: { id: string; title: string; desktop?: boolean }[];
+};
+
+export default function NavMenu({ links }: NavMenuProps) {
+  return (
+    <ol className={styles.menu}>
+      {links.map(({ id, title }) => (
+        <li className={styles.menu_item} key={id}>
+          <Link href={id}>{title}</Link>
+        </li>
+      ))}
+    </ol>
+  );
+}
