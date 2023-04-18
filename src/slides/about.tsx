7002e87d54b4745d0330a04b2062486c6ab271bc
Anshuman <anshuman844@gmail.com>
2023-04-18 22:37:06 +0530

    feat: Create about slide

---
+++ src/slides/about.tsx
@@ -0,0 +1,23 @@
+import SlideTitle from "@/components/slide-title";
+import slides from "@/styles/slides/Slides.module.css";
+import about from "@/styles/slides/About.module.css";
+import portfolio from "@/portfolio.json";
+
+export default function About({ id }: { id: string }) {
+  return (
+    <section className={slides.slide} id={id}>
+      <SlideTitle title="About Me" />
+      <div className={about.main}>
+        <div className={about.content}>
+          <div>
+            { portfolio.about.map((line, idx) => <p key={idx}>{line}</p>) }
+          </div>
+          <ul className={about.skills}>
+            { portfolio.skills.map((skill, idx) => <li key={idx}>{skill}</li>) }
+          </ul>
+        </div>
+        <div className={about.picture}></div>
+      </div>
+    </section>
+  );
+}
