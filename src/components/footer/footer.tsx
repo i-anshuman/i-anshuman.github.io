7e4d6a94e527d930c2de3a019dc88d872e3d5d73
Anshuman <anshuman844@gmail.com>
2023-09-15 00:42:21 +0530

    feat: Add sidebar links

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,30 +1,64 @@
 import portfolio from "@/portfolio.json";
 import { SocialButton } from "@/components/button";
+import { Sidebar } from "@/components/sidebar";
+import { Alignment } from "@/types";
 import styles from "@/styles/components/Footer.module.css";
 
 type FooterProps = {};
 
 export default function Footer(props: FooterProps) {
   return (
-    <footer className={styles.footer}>
-      <div className={styles.social}>
-        {
-          portfolio.social
-            .filter(({ hidden }) => !hidden)
-            .map(({title, url, username, icon}) => (
-              <SocialButton
-                key={title}
-                url={url}
-                username={username}
-                icon={icon}
-                aria-label={title}
-              />
-          ))
-        }
+    <>
+      <footer className={styles.footer}>
+        <div className={styles.container}>
+          <ul className={styles.social}>
+            {
+              portfolio.social
+                .filter(({ hidden }) => !hidden)
+                .map(({title, url, username, icon}) => (
+                  <li key={title}>
+                    <SocialButton
+                      url={url}
+                      username={username}
+                      icon={icon}
+                      aria-label={title}
+                    />
+                  </li>
+              ))
+            }
+          </ul>
+        </div>
+        <p className={styles.note}>
+          Developed with <i className="fa-solid fa-heart"></i> by {portfolio.firstname}
+        </p>
+      </footer>
+      <div className={styles.sidebar}>
+        <Sidebar alignment={Alignment.LEFT}>
+          <ul className={`${styles.social} ${styles.line}`}>
+            {
+              portfolio.social
+                .filter(({ hidden }) => !hidden)
+                .map(({title, url, username, icon}) => (
+                  <li key={title}>
+                    <SocialButton
+                      url={url}
+                      username={username}
+                      icon={icon}
+                      aria-label={title}
+                    />
+                  </li>
+              ))
+            }
+          </ul>
+        </Sidebar>
+        <Sidebar alignment={Alignment.RIGHT}>
+          <div className={`${styles.email} ${styles.line}`}>
+            <a href={`mailto:${portfolio.contact.email}`} aria-label="Send me a mail">
+              {portfolio.contact.email}
+            </a>
+          </div>
+        </Sidebar>
       </div>
-      <p className={styles.note}>
-        Developed with <i className="fa-solid fa-heart"></i> by {portfolio.fullname}
-      </p>
-    </footer>
+    </>
   );
 }
